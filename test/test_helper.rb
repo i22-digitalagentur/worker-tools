20a1ccc34be68eabb896e2fba115118d9bad9256
Patrick Smandzich <psmandzich@users.noreply.github.com>
2017-12-29 09:22:51 +0100

    added simplecov to test suite

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,9 +1,39 @@
-require 'minitest/autorun'
-require 'minitest/pride'
-require 'mocha/mini_test'
+require 'simplecov'
+SimpleCov.profiles.define 'lib' do
+  add_filter '/test/'
+  add_filter '/config/'
+  add_group 'Libraries', 'lib'
+end
+SimpleCov.start 'lib'
 require 'pry'
 
 Dir[File.join(__dir__, 'support/**/*.rb')].each { |f| require f }
 
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'worker_tools'
+
+require 'minitest/autorun'
+require 'minitest/pride'
+require 'mocha/mini_test'
+require 'active_record'
+
+ActiveRecord::Base.establish_connection adapter: 'sqlite3', database: ':memory:'
+
+load File.dirname(__FILE__) + '/schema.rb'
+require File.dirname(__FILE__) + '/models.rb'
+
+require 'database_cleaner'
+
+DatabaseCleaner.strategy = :transaction
+
+module Minitest
+  class Spec
+    before :each do
+      DatabaseCleaner.start
+    end
+
+    after :each do
+      DatabaseCleaner.clean
+    end
+  end
+end
--- worker_tools.gemspec
+++ worker_tools.gemspec
@@ -37,5 +37,6 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'pry'
   spec.add_development_dependency 'rake'
   spec.add_development_dependency 'rubocop'
+  spec.add_development_dependency 'simplecov'
   spec.add_development_dependency 'sqlite3'
 end
